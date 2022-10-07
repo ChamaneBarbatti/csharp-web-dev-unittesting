e61fb808c1d927664f627b5343f179401f9040dd
Chamane Barbatti <chamane@sbcglobal.net>
2022-10-07 02:17:57 -0500

    Finished

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,14 +1,65 @@
+using CarNS;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace CarTests
 {
+
+
+    [TestClass]
     public class CarTests
     {
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
+        Car test_car;
+
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+
+        }
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            Car test_car = new Car("Toyota", "Prius", 10, 50);
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
+
+
+        [TestMethod]
+        public void TestGasTankAfterDriving()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+
+        }
+
+        [TestMethod]
+        
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(501);
+            Assert.AreEqual(test_car.GasTankLevel, 0, .001);
+
+        }
+        [TestMethod]
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+        }
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
         //TODO: constructor sets gasTankLevel properly
         //TODO: gasTankLevel is accurate after driving within tank range
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
         //TODO: can't have more gas than tank size, expect an exception
-
     }
 }
+
